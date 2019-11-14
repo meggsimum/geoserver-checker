b5b0142e12343af11d18b3942cc1b17a078dd22c
Christian Mayer <chris@meggsimum.de>
2019-11-14 09:38:33 +0100

    Improve GeoServer URL configuration
    Interprets env var 'GEOSEVER_BASEURL' to directly inject a complete URL
    to a GeoServer instance. If not provided 'GEOSEVER_PROTOCOLL', 'GEOSEVER_HOST',
    'GEOSEVER_PORT' and 'GEOSEVER_PATH' are interpeted with common defaults
    (=> http://localhost:8080/geoserver/)

--- index.js
+++ index.js
@@ -7,13 +7,25 @@
 const puppeteer = require('puppeteer');
 const screenshot = 'geoserver.png';
 
+let geoserverBaseUrl;
+if (process.env.GEOSEVER_BASEURL) {
+  geoserverBaseUrl = process.env.GEOSEVER_BASEURL;
+} else {
+  const protocoll = process.env.GEOSEVER_PROTOCOLL || 'http';
+  const host = process.env.GEOSEVER_HOST || 'localhost';
+  const port = process.env.GEOSEVER_PORT || '8080';
+  const path = process.env.GEOSEVER_PATH || 'geoserver';
+
+  geoserverBaseUrl = 'http://' + host + ':' + port + '/' + path + '/';
+}
+
 const user = process.env.GEOSEVER_USER || 'admin';
 const pwd = process.env.GEOSEVER_PWD || 'geoserver';
-const port = process.env.GEOSEVER_PORT || '8080';
+
 (async () => {
   const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
-  await page.goto('http://localhost:' + port + '/geoserver/web/');
+  await page.goto(geoserverBaseUrl + 'web');
   await page.type('#username', user);
   await page.type('#password', pwd);
   await page.keyboard.press('Enter');
@@ -21,4 +33,4 @@ const port = process.env.GEOSEVER_PORT || '8080';
   await page.screenshot({ path: screenshot });
   browser.close();
   console.log('See screenshot: ' + screenshot);
-})()
+})();
